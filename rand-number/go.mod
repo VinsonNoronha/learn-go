6203b759c366de78d4dc6db277daa11efea62730
Vinson Noronha <vinson.noronha@syncier.com>
2023-02-17 20:47:00 +0100

    packages in go

---
+++ rand-number/go.mod
@@ -0,0 +1,3 @@
+module vinsonnoronha.de/random-number
+
+go 1.18
---
+++ rand-number/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"vinsonnoronha.de/random-number/random"
+)
+
+func main() {
+    fmt.Printf("Your random number is %d!\n", random.Number())
+}
